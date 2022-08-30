06f527943ba55b949ce435d53f211150e5eeaa46
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-08-30 13:27:41 +0200

    fix: remove need for access tokens

--- js/github.js
+++ js/github.js
@@ -1,5 +1,3 @@
-var access_token = ['f1aff3072a385154ffe18aed3b893aa46ce8577c', '2e71ec1017dda2220ccba0f6922ecefd9ea44ac7', 'bfaeb43c92d3e2f534745a6df977f68b64dc7c55', '7731a6f681df209067f597a04822c7abf0425c9a', 'b59d67cf0ba63a89b87ebad75d4ec1e08d9f2e43', 'daf69c16bec62f7c2faf5e5a7f445a4823f2f531'];
-
 var width = window.innerWidth;
 var height = window.innerHeight;
 var margin = 20;
@@ -10,24 +8,24 @@ var highlighted = [];
 var exclude;
 
 function checkQuery() {
-  var owner = fromQuery('owner');
-  var repo = fromQuery('repo');
-  var branch = fromQuery('branch');
-  exclude = fromQuery('exclude');
-  if (exclude){
-    exclude = exclude.split(',');
+  var owner = fromQuery("owner");
+  var repo = fromQuery("repo");
+  var branch = fromQuery("branch");
+  exclude = fromQuery("exclude");
+  if (exclude) {
+    exclude = exclude.split(",");
   }
 
   if (owner && repo) {
-    $('input#owner').val(owner);
-    $('input#repo').val(repo);
+    $("input#owner").val(owner);
+    $("input#repo").val(repo);
     getRepo(branch);
   }
 }
 
 function fromQuery(value) {
   var qs = window.location.search;
-  var re = new RegExp(value + '=([^&]*)');
+  var re = new RegExp(value + "=([^&]*)");
   var match = re.exec(qs);
   if (match) {
     return match[1];
@@ -37,63 +35,88 @@ function fromQuery(value) {
 }
 
 function getRepo(branch) {
-  var owner = $('input#owner').val().trim(),
-      repo = $('input#repo').val().trim(),
-    queryString = 'owner=' + owner + '&repo=' + repo + (branch ? '&branch=' + branch : '') + (exclude ? '&exclude=' + exclude.join() : '');
-  window.history.pushState({}, '', window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + queryString);
-  $('img#logo').attr('src', 'images/hex-loader.gif');
+  var owner = $("input#owner").val().trim(),
+    repo = $("input#repo").val().trim(),
+    queryString =
+      "owner=" +
+      owner +
+      "&repo=" +
+      repo +
+      (branch ? "&branch=" + branch : "") +
+      (exclude ? "&exclude=" + exclude.join() : "");
+  window.history.pushState(
+    {},
+    "",
+    window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?" +
+      queryString
+  );
+  $("img#logo").attr("src", "images/hex-loader.gif");
 
   $.ajax({
-    url: "https://api.github.com/repos/" + owner + "/" + repo + (branch ? "/branches/" + branch : "/commits"),
-    data: {
-      access_token: access_token[Math.floor(Math.random() * access_token.length)]
-    },
+    url:
+      "https://api.github.com/repos/" +
+      owner +
+      "/" +
+      repo +
+      (branch ? "/branches/" + branch : "/commits"),
     success: function (data) {
-      $('header').show();
-      $('form.start').removeClass('start');
-      $('header p').remove();
-      $('footer').addClass('shift');
-      $('#prompt').addClass('fade');
+      $("header").show();
+      $("form.start").removeClass("start");
+      $("header p").remove();
+      $("footer").addClass("shift");
+      $("#prompt").addClass("fade");
       var sha = branch ? data.commit.sha : data[0].sha,
-        url = "https://api.github.com/repos/" + owner + "/" + repo + "/git/trees/" + sha + "?recursive=1&access_token=" + access_token[Math.floor(Math.random() * access_token.length)];
+        url =
+          "https://api.github.com/repos/" +
+          owner +
+          "/" +
+          repo +
+          "/git/trees/" +
+          sha +
+          "?recursive=1";
       init(url);
     },
     error: function (request, status, error) {
-      $('img#logo').attr('src', 'images/logo.png');
-      alert('Error loading repo: ' + request.statusText);
-    }
+      $("img#logo").attr("src", "images/logo.png");
+      alert("Error loading repo: " + request.statusText);
+    },
   });
 }
 
 var color = d3.scale.category20b();
 
-var force = d3.layout.force()
+var force = d3.layout
+  .force()
   .gravity(0.2)
   .charge(-220)
   .size([width, height])
   .linkStrength(0.9)
   .linkDistance(function (d) {
-    return d.source.type === 'tree' && d.target.type === 'tree' ? 1 : 10;
+    return d.source.type === "tree" && d.target.type === "tree" ? 1 : 10;
   })
   .on("tick", tick);
 
-var outer = d3.select("div#graph").append("svg")
+var outer = d3
+  .select("div#graph")
+  .append("svg")
   .call(d3.behavior.zoom().on("zoom", rescale))
   .on("dblclick.zoom", null)
   .attr("width", width)
   .attr("height", height)
   .attr("pointer-events", "all");
 
-var svg = outer.append('svg:g');
+var svg = outer.append("svg:g");
 
 //Rescale function, called on zoom event
 function rescale() {
   trans = d3.event.translate;
   scale = d3.event.scale;
-  svg.attr("transform",
-    "translate(" + trans + ")" + " scale(" + scale + ")");
+  svg.attr("transform", "translate(" + trans + ")" + " scale(" + scale + ")");
 }
-
 
 var link = svg.selectAll(".link"),
   node = svg.selectAll(".node");
@@ -105,11 +128,11 @@ function init(url) {
     if (error) {
       return console.warn(error);
     }
-    $('header').hide();
+    $("header").hide();
     json.tree.forEach(function (o) {
-      var indexSlash = o.path.lastIndexOf('/');
+      var indexSlash = o.path.lastIndexOf("/");
       if (indexSlash < 0) {
-        o.parent = 'root';
+        o.parent = "root";
         o.filename = o.path;
         o.name = o.path;
       } else {
@@ -119,12 +142,12 @@ function init(url) {
       }
     });
     json.tree.unshift({
-      "path": "root",
-      "type": "tree",
-      "size": 0,
-      "parent": null,
-      "filename": "root",
-      "name": "root"
+      path: "root",
+      type: "tree",
+      size: 0,
+      parent: null,
+      filename: "root",
+      name: "root",
     });
     var dataMap = json.tree.reduce(function (map, node) {
       map[node.path] = node;
@@ -134,11 +157,11 @@ function init(url) {
       // add to parent
       var parent = dataMap[node.parent];
       if (parent) {
-        if (exclude && exclude.indexOf(parent.path) > -1){
-          (parent._children || (parent._children = [])).push(node);}
+        if (exclude && exclude.indexOf(parent.path) > -1) {
+          (parent._children || (parent._children = [])).push(node);
+        }
         // create child array if it doesn't exist
-        else
-          (parent.children || (parent.children = [])).push(node);
+        else (parent.children || (parent.children = [])).push(node);
       } else {
         // parent is null or missing
         treeData.push(node);
@@ -155,10 +178,7 @@ function update() {
     links = d3.layout.tree().links(nodes);
 
   // Restart the force layout.
-  force
-    .nodes(nodes)
-    .links(links)
-    .start();
+  force.nodes(nodes).links(links).start();
 
   // Update the links…
   link = link.data(links, function (d) {
@@ -169,7 +189,9 @@ function update() {
   link.exit().remove();
 
   // Enter any new links.
-  link.enter().insert("line", ".node")
+  link
+    .enter()
+    .insert("line", ".node")
     .attr("class", "link")
     .attr("x1", function (d) {
       return d.source.x;
@@ -185,17 +207,29 @@ function update() {
     });
 
   // Update the nodes…
-  node = node.data(nodes, function (d) {
-    return d.id;
-  }).style("fill", function (d) {
-    return d.name === "root" ? '#f00' : d.type === "tree" ? "#ccc" : color(d.filename.lastIndexOf('.') >= 0 ? d.filename.substring(d.filename.lastIndexOf('.')) : "others");
-  });
+  node = node
+    .data(nodes, function (d) {
+      return d.id;
+    })
+    .style("fill", function (d) {
+      return d.name === "root"
+        ? "#f00"
+        : d.type === "tree"
+        ? "#ccc"
+        : color(
+            d.filename.lastIndexOf(".") >= 0
+              ? d.filename.substring(d.filename.lastIndexOf("."))
+              : "others"
+          );
+    });
 
   // Exit any old nodes.
   node.exit().remove();
 
   // Enter any new nodes.
-  node.enter().append("circle")
+  node
+    .enter()
+    .append("circle")
     .attr("class", "node")
     .attr("cx", function (d) {
       return d.x;
@@ -204,72 +238,94 @@ function update() {
       return d.y;
     })
     .attr("r", function (d) {
-      return d.name === "root" ? 10 : d.type === 'tree' ? 7 : Math.log(d.size + 1) || 3;
+      return d.name === "root"
+        ? 10
+        : d.type === "tree"
+        ? 7
+        : Math.log(d.size + 1) || 3;
     })
     .style("fill", function (d) {
-      return d.name === "root" ? '#f00' : d.type === "tree" ? "#ccc" : color(d.filename.lastIndexOf('.') >= 0 ? d.filename.substring(d.filename.lastIndexOf('.')) : "others");
+      return d.name === "root"
+        ? "#f00"
+        : d.type === "tree"
+        ? "#ccc"
+        : color(
+            d.filename.lastIndexOf(".") >= 0
+              ? d.filename.substring(d.filename.lastIndexOf("."))
+              : "others"
+          );
     })
     .on("click", click)
-    .on('mouseover', function (d) {
+    .on("mouseover", function (d) {
       var ancestors = d.path;
-      link.style('stroke-width', function (l) {
-        if (ancestors && d.name == l.target.name || ancestors.indexOf(l.target.name + '/') >= 0)
+      link.style("stroke-width", function (l) {
+        if (
+          (ancestors && d.name == l.target.name) ||
+          ancestors.indexOf(l.target.name + "/") >= 0
+        )
           return 4;
-        else
-          return 2;
+        else return 2;
       });
-      link.style('stroke', function (l) {
-        if (ancestors && d.name == l.target.name ||  ancestors.indexOf(l.target.name + '/') >= 0)
+      link.style("stroke", function (l) {
+        if (
+          (ancestors && d.name == l.target.name) ||
+          ancestors.indexOf(l.target.name + "/") >= 0
+        )
           return "#ff8080";
-        else
-          return '#9ecae1';
+        else return "#9ecae1";
       });
-      node.style('stroke', function (n) {
-        if (ancestors && ancestors.indexOf(n.name) >= 0 || n.name == "root")
+      node.style("stroke", function (n) {
+        if ((ancestors && ancestors.indexOf(n.name) >= 0) || n.name == "root")
           return "#ff8080";
-        else
-          return '#3182bd';
+        else return "#3182bd";
       });
-      link.each(function(l){
-        if (ancestors && d.name == l.target.name ||  ancestors.indexOf(l.target.name + '/') >= 0) {
+      link.each(function (l) {
+        if (
+          (ancestors && d.name == l.target.name) ||
+          ancestors.indexOf(l.target.name + "/") >= 0
+        ) {
           this.parentNode.appendChild(this);
           highlighted.push(this);
         }
       });
-      node.each(function(n){
-        if (ancestors && ancestors.indexOf(n.name) >= 0 || n.name == "root")
+      node.each(function (n) {
+        if ((ancestors && ancestors.indexOf(n.name) >= 0) || n.name == "root")
           this.parentNode.appendChild(this);
       });
     })
-    .on('mouseout', function () {
+    .on("mouseout", function () {
       while (highlighted.length > 0) {
         var l = highlighted.pop();
-        var firstChild = l.parentNode.firstChild; 
-        if (firstChild) { 
-            l.parentNode.insertBefore(l, firstChild); 
-        } 
+        var firstChild = l.parentNode.firstChild;
+        if (firstChild) {
+          l.parentNode.insertBefore(l, firstChild);
+        }
       }
-      link.style('stroke-width', 2);
-      link.style('stroke', '#9ecae1');
-      node.style('stroke', '#3182bd');
+      link.style("stroke-width", 2);
+      link.style("stroke", "#9ecae1");
+      node.style("stroke", "#3182bd");
     });
 
   //Update legend
-  var legend = outer.selectAll(".legend")
+  var legend = outer
+    .selectAll(".legend")
     .data(color.domain())
-    .enter().append("g")
+    .enter()
+    .append("g")
     .attr("class", "legend")
     .attr("transform", function (d, i) {
       return "translate(-10," + (i * 20 + 10) + ")";
     });
 
-  legend.append("rect")
+  legend
+    .append("rect")
     .attr("x", width - 18)
     .attr("width", 18)
     .attr("height", 18)
     .style("fill", color);
 
-  legend.append("text")
+  legend
+    .append("text")
     .attr("x", width - 24)
     .attr("y", 9)
     .attr("dy", ".35em")
@@ -282,23 +338,22 @@ function update() {
     return "<p class='filename'>" + name + "</p>";
   };
 
-
-  svg.selectAll("circle.node")
-    .each(function (v) {
-      $(this).tipsy({
-        gravity: "w",
-        opacity: 1,
-        html: true,
-        title: function () {
-          var d = this.__data__;
-          return styleTooltip(d.filename);
-        }
-      });
+  svg.selectAll("circle.node").each(function (v) {
+    $(this).tipsy({
+      gravity: "w",
+      opacity: 1,
+      html: true,
+      title: function () {
+        var d = this.__data__;
+        return styleTooltip(d.filename);
+      },
     });
+  });
 }
 
 function tick() {
-  link.attr("x1", function (d) {
+  link
+    .attr("x1", function (d) {
       return d.source.x;
     })
     .attr("y1", function (d) {
@@ -311,7 +366,8 @@ function tick() {
       return d.target.y;
     });
 
-  node.attr("cx", function (d) {
+  node
+    .attr("cx", function (d) {
       return d.x;
     })
     .attr("cy", function (d) {
@@ -360,7 +416,7 @@ function flatten(root) {
     if (!node.id) node.id = ++i;
     nodes.push(node);
   }
-  
+
   function recurse_exclude(node) {
     if (node.children) node.children.forEach(recurse_exclude);
     else if (node._children) node._children.forEach(recurse_exclude);
